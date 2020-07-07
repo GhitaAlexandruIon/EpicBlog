e512c13313aac220d14c1ce6b7ebab19969df108
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-07 10:23:49 +0300

    instaled django-debug-toolbar

--- EpicBlog/settings.py
+++ EpicBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9waf0^gmlcw%^^5@%o*1vbl1l)wtgmq9e8$-nt5(^2)fmuro4k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
