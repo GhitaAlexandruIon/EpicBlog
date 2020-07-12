c416976fde21714477a7636cb73f2eb7f4bca8ac
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-12 11:29:28 +0300

    UnLike button

--- epicApp/views.py
+++ epicApp/views.py
@@ -30,8 +30,12 @@ class PostDetailView(DetailView):
         context = super(PostDetailView, self).get_context_data(*args, **kwargs)
         votes = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = votes.total_likes()
+        liked = False
+        if votes.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context['cat_menu'] = cat_menu
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 
@@ -72,5 +76,11 @@ def CategoryListView(request):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('detail', args=[str(pk)]))
