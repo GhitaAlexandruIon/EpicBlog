73afc5543da50d62925e5a5e729a1355cfadf8ae
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-12 11:15:10 +0300

    like button

--- epicApp/views.py
+++ epicApp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-
+from django.http import HttpResponseRedirect
 from .forms import PostForm
 from .models import Post, Category
 
@@ -24,6 +24,15 @@ class HomeView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        votes = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = votes.total_likes()
+        context['cat_menu'] = cat_menu
+        context['total_likes'] = total_likes
+        return context
 
 
 class AddPostView(CreateView):
@@ -59,3 +68,9 @@ def CategoryView(request, cats):
 def CategoryListView(request):
     cat_menu_list = Category.objects.all()
     return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('detail', args=[str(pk)]))
