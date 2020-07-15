061da3a2e83c674c11b318be6285d37998ed78d8
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-15 21:08:49 +0300

    profile page

--- epicApp/models.py
+++ epicApp/models.py
@@ -36,3 +36,18 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('detail', args=(str(self.id)))
         return reverse('home')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    website = models.CharField(max_length=255, null=True, blank=True)
+    github = models.CharField(max_length=255, null=True, blank=True)
+    linkedin = models.CharField(max_length=255, null=True, blank=True)
+    facebook = models.CharField(max_length=255, null=True, blank=True)
+    instagram = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.user)
