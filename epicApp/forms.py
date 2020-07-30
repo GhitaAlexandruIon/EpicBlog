8ca7e3d6926a088c6727aa5213f9feccc2b916b6
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-30 22:17:35 +0300

    tri fix error 3

--- epicApp/forms.py
+++ epicApp/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from epicApp.models import Post, Category, Comment
 
-choices = Category.objects.all().values_list('name', 'name')
-choice_list = []
-for item in choices:
-    choice_list.append(item)
+# choices = Category.objects.all().values_list('name', 'name')
+# choice_list = []
+# for item in choices:
+#     choice_list.append(item)
 
 
 class CommentForm(forms.ModelForm):
