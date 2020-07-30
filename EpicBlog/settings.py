f9fd19a585b4075c24644c2fb7f6de8886d57b6d
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-30 22:24:26 +0300

    static files dir error

--- EpicBlog/settings.py
+++ EpicBlog/settings.py
@@ -124,11 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static_root')
-MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
