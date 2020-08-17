ad2adca5cd46fc29e0afef0e6ced5e841e5b5b17
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-08-18 01:01:36 +0300

    error

--- epicApp/forms.py
+++ epicApp/forms.py
@@ -2,17 +2,18 @@ from django import forms
 from django.db.utils import IntegrityError, ProgrammingError, OperationalError
 from epicApp.models import Post, Category, Comment
 
-try:
-    choices = Category.objects.all().values_list('name', 'name')
-    choice_list = []
-    for item in choices:
-        choice_list.append(item)
-except IntegrityError as e:
-    print(e)
-except ProgrammingError as f:
-    print(f)
-except OperationalError as d:
-    print(d)
+# try:
+#     choices = Category.objects.all().values_list('name', 'name')
+#     choice_list = []
+#     for item in choices:
+#         choice_list.append(item)
+# except IntegrityError as e:
+#     print(e)
+# except ProgrammingError as f:
+#     print(f)
+# except OperationalError as d:
+#     print(d)
+# choices=choice_list,
 
 
 class CommentForm(forms.ModelForm):
@@ -36,7 +37,7 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'id': 'user', 'value': '', 'type': 'hidden'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
         }
